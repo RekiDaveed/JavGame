e5d04d7d05b2c12918d75af0083eaf55a0522654
David <daveedtee5@gmail.com>
2026-01-05 00:45:41 -0500

    imageicon frame

--- src/Main/JavGame.java
+++ src/Main/JavGame.java
@@ -3,19 +3,32 @@ package Main;
 import UI.Game;
 
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
 
 public class JavGame {
-     static void main(String[] args) {
+
+    static BufferedImage ImageIcon;
+     public static void main(String[] args) throws IOException {
 
         JFrame frame = new JFrame("Type Soul is Real");
         Game game = new Game();
 
+        ImageIcon = ImageIO.read(Objects.requireNonNull(JavGame.class.getResourceAsStream("/Pack/FreeUi/img_1.png")));
+
+        frame.setIconImage(ImageIcon);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.add(game);
         frame.pack();
         frame.setVisible(true);
+
 
         game.GameStart();
 
